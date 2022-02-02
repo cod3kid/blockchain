3c573abf4a499c9024c1d0896897d692e59eb7f2
cod3kid <djsufail@gmail.com>
2022-02-02 08:21:58 +0530

    Fetched pregenerated accounts by Ganache network

--- 1-contract-deployment/test/Inbox.test.js
+++ 1-contract-deployment/test/Inbox.test.js
@@ -2,3 +2,13 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+
+beforeEach(() => {
+  web3.eth.getAccounts().then((fetchedAccounts) => {
+    console.log(fetchedAccounts);
+  });
+});
+
+describe("Inbox", () => {
+  it("deploys a contracts", () => {});
+});
