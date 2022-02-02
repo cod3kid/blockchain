ab063a293554e5e9f0f9ef17bd390c2c44f326e3
cod3kid <djsufail@gmail.com>
2022-02-02 08:36:45 +0530

    SC Deployment

--- 1-contract-deployment/test/Inbox.test.js
+++ 1-contract-deployment/test/Inbox.test.js
@@ -2,13 +2,28 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const { bytecode, interface } = require("../compile");
 
-beforeEach(() => {
-  web3.eth.getAccounts().then((fetchedAccounts) => {
-    console.log(fetchedAccounts);
-  });
+let accounts, inbox;
+beforeEach(async () => {
+  //fetch all pre-generated accounts by ganache
+  accounts = await web3.eth.getAccounts();
+
+  // deploy the smart contract
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hello World"],
+    })
+    .send({
+      from: accounts[0],
+      gas: "1000000",
+    });
 });
 
 describe("Inbox", () => {
-  it("deploys a contracts", () => {});
+  it("deploys a contracts", () => {
+    console.log(inbox);
+  });
 });
