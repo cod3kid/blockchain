0ffd4541b8f01a783b4b7af0dcb5ca96851db9ad
cod3kid <djsufail@gmail.com>
2022-02-03 21:54:02 +0530

    Contract deployed to Rinkeby Ethereum Network

--- 1-contract-deployment/deploy.js
+++ 1-contract-deployment/deploy.js
@@ -8,3 +8,22 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("Get first account account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi Rinkeby !"],
+    })
+    .send({
+      gas: "1000000",
+      from: accounts[0],
+    });
+
+  console.log("Contract deployed to", result.options.address);
+};
+
+deploy();
