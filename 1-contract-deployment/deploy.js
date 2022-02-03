d07f05036101d7666e4cd17bdb44797e078482be
cod3kid <djsufail@gmail.com>
2022-02-03 20:57:54 +0530

    Truffle Provider Setup

--- 1-contract-deployment/deploy.js
+++ 1-contract-deployment/deploy.js
@@ -1,1 +1,10 @@
-// deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "possible milk pole gap exhaust liar scissors silent select tree monitor dad",
+  "https://rinkeby.infura.io/v3/d8cc776b5fcd45f1a8480203d0beb116"
+);
+
+const web3 = new Web3(provider);
