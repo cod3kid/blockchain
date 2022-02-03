18acf7d7edf7218e0352b875df24dcd28aa0cad9
cod3kid <djsufail@gmail.com>
2022-02-03 08:36:04 +0530

    Default Message Test

--- 1-contract-deployment/test/Inbox.test.js
+++ 1-contract-deployment/test/Inbox.test.js
@@ -5,6 +5,8 @@ const web3 = new Web3(ganache.provider());
 const { bytecode, interface } = require("../compile");
 
 let accounts, inbox;
+const INITIAL_STRING = "Hello World";
+
 beforeEach(async () => {
   //fetch all pre-generated accounts by ganache
   accounts = await web3.eth.getAccounts();
@@ -14,7 +16,7 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
-      arguments: ["Hello World"],
+      arguments: [INITIAL_STRING],
     })
     .send({
       from: accounts[0],
@@ -25,5 +27,10 @@ beforeEach(async () => {
 describe("Inbox", () => {
   it("deploys a contracts", () => {
     assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message.call();
+    assert.equal(message, INITIAL_STRING);
   });
 });
