2ae1a4c49434c7d5813b8c7cacfb0124394e2cff
cod3kid <djsufail@gmail.com>
2022-02-06 10:41:48 +0530

    Setting up contract owner as manager when the instance is deployed

---
+++ 2-lottery-smart-contract/contracts/Lottery.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    address public manager;
+    address[] public players;
+    
+    function Lottery() public {
+        manager = msg.sender;
+    }
+    
+
+}   
