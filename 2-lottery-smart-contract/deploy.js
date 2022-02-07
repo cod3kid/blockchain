c9bc3f74cd1ee0f51a33642e094f7738a75fd526
cod3kid <djsufail@gmail.com>
2022-02-07 09:06:20 +0530

    Lottery Contract DApp

--- 2-lottery-smart-contract/deploy.js
+++ 2-lottery-smart-contract/deploy.js
@@ -17,6 +17,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: "1000000", from: accounts[0] });
 
+  console.log("Interface", interface);
   console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
---
+++ 3-dapp-lottery-react/src/lottery.js
@@ -0,0 +1,58 @@
+import web3 from "./web3";
+
+const address = "0x65d787d739bcf0766D49D30BfD500305E4ff91f3";
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: "manager",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "getPlayers",
+    outputs: [{ name: "", type: "address[]" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    payable: true,
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ name: "", type: "address" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+];
+// Local Contract Instance
+export default new web3.eth.Contract(abi, address);
